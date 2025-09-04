0759f18b26532df44067b9ccc3063ca0e145ab11
reethuo <152209084+reethuo@users.noreply.github.com>
2025-09-04 15:42:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@
 # This block retrieves environment variables.
 # The 'var' prefix is a convention for referring to variables.
 # Terraform variables are defined in variables.tf
-
+provider "google" {
+  project = var.project
+  zone    = var.region
+}
 resource "google_compute_instance" "vm" {
   name         = "vm-${var.project}"
   machine_type = "e2-micro"
